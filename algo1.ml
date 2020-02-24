08d64fdb69bc08d5e51963826048d35ef165d9ea
Anis BOUCHEKHIMA <bouchekhima.anis@gmail.com>
2020-02-24 11:46:12 +0100

    add graph connexite (algo1)

--- algo1.ml
+++ algo1.ml
@@ -19,93 +19,55 @@ let inverse_graphe graphe = List.fold_left (fun a (s, succ) -> (s, (predecesseur
 inverse_graphe graphe1;;
 
 (* 4. Efectuer le parcours en profondeur d’un graphe et retourner la liste des sommets parcourus en ordre sufxe (inversé). *)
+(* parcours du graphe à partir d'un sommet x *)
+(* visited est la liste des sommets déja visités *)
+let rec parcours_sommet x visited graphe =
+    let (l, v) = List.fold_left (
+                                     (* on ignore les sommets déjà visités *)
+                     fun (l, v) e -> if (List.mem e v) then (l, v)
+
+                                     (* on parcours le graphe à partir du successeurs `e` *)
+                                     else let (rl, rv) = parcours_sommet e v graphe
+
+                                          (* on ajoute la liste des sommets parcourus à partir de `e` à la fin *)
+                                          (* on mets à jour la liste des sommets visités *)
+                                          in  (rl @ l, rv)
+                 )
+
+                 (* on commence par ajouter x à la liste des sommet visités *)
+                 ([], x::visited)
+
+                 (* on parcours les successeurs du sommet x dans le graphe *)
+                 (successeurs x graphe)
+
+    (* on ajoute le sommet x au début de la liste des sommets parcourus *)
+    in (x::l, v);;
+
 let parcours_prof graphe =
-	(* on parcours le graphe à partir du sommet x *)
-	(* visited est la liste des sommets déja visités *)
-    let rec parcours x visited =
-        let (l, v) = List.fold_left (
-                                         (* on ignore les sommets déjà visités *)
-                         fun (l, v) e -> if (List.mem e v) then (l, v)
-
-                                         (* on parcours le graphe à partir du successeurs `e` *)
-                                         else let (rl, rv) = parcours e v
-
-                                              (* on ajoute la liste des sommets parcourus à partir de `e` à la fin *)
-                                              (* on mets à jour la liste des sommets visités *)
-                                              in  (rl @ l, rv)
-                     )
-
-                     (* on commence par ajouter x à la liste des sommet visités *)
-                     ([], x::visited)
-
-					 (* on parcours les successeurs du sommet x dans le graphe *)
-                     (successeurs x graphe)
-
-		(* on ajoute le sommet x au début de la liste des sommets parcourus *)
-        in (x::l, v)
-
 	(* pour chaque sommet du graphe *)
-    in fst (List.fold_left (
-                            (* si le sommet n'est pas visité,
-                               on parcours le graphe à partir de ce sommet *)
-            fun (l, v) s -> if List.mem s v then (l, v)
-                            else let (rl, rv) = parcours s v
-                            in       (rl @ l, rv)
-        )
-        ([], [])
-        (sommets graphe));;
+    fst (List.fold_left (
+                        (* si le sommet n'est pas visité,
+                           on parcours le graphe à partir de ce sommet *)
+        fun (l, v) s -> if List.mem s v then (l, v)
+                        else let (rl, rv) = parcours_sommet s v graphe
+                        in       (rl @ l, rv)
+    )
+    ([], [])
+    (sommets graphe));;
 
-parcours_prof graphe1;;
-parcours_prof (inverse_graphe graphe1);;
 (* 5. Rechercher les composantes fortement connexes par parcours du graphe et de son inverse. *)
+let connexites graphe =
+    let inv_graph = inverse_graphe graphe
+    in let res =
+        List.fold_left ( fun (cnx, visited) e ->
+            if (List.mem e visited) then (cnx, visited)
+            else let (composant, v) = parcours_sommet e visited inv_graph
+                 in  (composant::cnx, v)
+        )
 
+        ([], [])
+        (parcours_prof graphe)
 
+    in fst res;;
 
-
-
-
-
-
-
-
-(*
-
-
-
-
-	
-	
-
-	L = parcours_prof(graphe)
-	
-	k = 0
-	h = 0
-
-	marque = [false, false, ..., false]
-	comp   = [0, 0, ..., 0]
-	
-
-
-	foreach (let sommet in L.inverse()) {
-		if (!marque[sommet]) {
-			k++
-			parcours(sommet, graphe, marque, comp, k)
-		}	
-	}
-
-
-	graphe  -> [2; 4; 3; 1; 8; 6; 7; 5]
-	inverse -> [7; 8; 1; 5; 4; 2; 3; 6]
-
-	
-*)
-
-
-
-
-
-
-
-
-
-
+connexites graphe1;;
